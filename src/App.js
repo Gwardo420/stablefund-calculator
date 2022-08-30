8197eb3d13d7d06bae14aebe3f4da33bf9ad178c
Gwardo420's Projects <torreykimbrough@gmail.com>
2022-08-30 16:09:40 -0400

    Creator div update

--- src/App.js
+++ src/App.js
@@ -393,6 +393,34 @@ function App() {
           Gas Prices Coming Soon!
         </div>
       </div>
+
+      <div className="stable__div">
+        <header className="App-header">
+          Creator
+        </header>
+
+        <div className="stable_div" style={{ paddingBottom: '20px' }}>
+          <img className="creator_image" style={{ borderRadius: 10, }} src="https://avatars.githubusercontent.com/u/25094504?v=4"></img>
+        </div>
+
+        <div className="display__div__text__big">
+          My name is Gwardo420! I am a Front End Developer for a few projects. I created this tool to help the StableFund community calculate how much they would earn if the compound their investment. 
+        </div>
+
+        <div onClick={() => window.open("https://www.twitter.com/devgwardo")} style={{ display: 'flex', justifyContent: 'space-evenly', paddingTop: 20 }}>
+          <div className="social_tag" style={{ display: 'flex', alignItems: 'center', cursor: 'pointer' }}>
+            <img style={{ marginRight: 5 }} height={20} src="https://iconmonstr.com/wp-content/g/gd/makefg.php?i=../releases/preview/2012/png/iconmonstr-twitter-1.png&r=0&g=0&b=0"></img>
+
+            Twitter
+          </div>
+
+          <div onClick={() => window.open("https://github.com/Gwardo420/stablefund-calculator")} className="social_tag" style={{ display: 'flex', alignItems: 'center', cursor: 'pointer' }}>
+            <img style={{ marginRight: 5 }} height={20} src="https://iconmonstr.com/wp-content/g/gd/makefg.php?i=../releases/preview/2012/png/iconmonstr-github-1.png&r=0&g=0&b=0"></img>
+
+            GitHub
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
