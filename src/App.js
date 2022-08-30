2b382890a2b685e62536fa5af26351aaca402be4
Gwardo420's Projects <torreykimbrough@gmail.com>
2022-08-30 11:02:06 -0400

    Price Updater

--- src/App.js
+++ src/App.js
@@ -39,6 +39,7 @@ function App() {
   const [bnbPrice, setBNBPrice] = useState(0);
   const [maticPrice, setMaticPrice] = useState(0);
   const [busdPrice, setBUSDPrice] = useState(0);
+  const [stableOne, setStableOnePrice] = useState(0);
 
   const selection_update = async (selection) => {
     if(selection.toUpperCase() === "BNB") {
@@ -64,6 +65,11 @@ function App() {
     await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=binance-usd&vs_currencies=usd').then((coin_gecko_results) => {
       console.log(coin_gecko_results.data['binance-usd'].usd)
       setBUSDPrice(coin_gecko_results.data['binance-usd'].usd)
+    })
+
+    await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=stable-one-rocket&vs_currencies=usd').then((coin_gecko_results) => {
+      console.log(coin_gecko_results.data)
+      setStableOnePrice(coin_gecko_results.data['stable-one-rocket'].usd)
     })
   }
 
@@ -275,9 +281,10 @@ function App() {
 
       <Marquee pauseOnHover={true} gradient={false} speed={50}>
         <div className="display__div__prices">
-          <span className="days__compound" style={{ color: 'lightblue' }}><img src="https://seeklogo.com/images/B/binance-coin-bnb-logo-97F9D55608-seeklogo.com.png" height={20} width={20} style={{ marginTop: 'auto', marginBottom: 'auto', marginRight: '5px' }}></img> BNB ${bnbPrice}</span> 
-          <span className="days__compound" style={{ color: 'lightblue' }}><img src="https://seeklogo.com/images/P/polygon-matic-logo-1DFDA3A3A8-seeklogo.com.png" height={20} width={20} style={{ marginTop: 'auto', marginBottom: 'auto', marginRight: '5px' }}></img> MATIC ${maticPrice}</span>
-          <span className="days__compound" style={{ color: 'lightblue' }}><img src="https://seeklogo.com/images/B/binance-coin-bnb-logo-CD94CC6D31-seeklogo.com.png" height={20} width={20} style={{ marginTop: 'auto', marginBottom: 'auto', marginRight: '5px' }}></img> BUSD ${busdPrice}</span>
+          <span className="days__compound" style={{ color: 'lightblue', width: 250 }}><img src="https://seeklogo.com/images/B/binance-coin-bnb-logo-97F9D55608-seeklogo.com.png" height={20} width={20} style={{ marginTop: 'auto', marginBottom: 'auto', marginRight: '5px' }}></img> BNB ${bnbPrice}</span> 
+          <span className="days__compound" style={{ color: 'lightblue', width: 250 }}><img src="https://seeklogo.com/images/P/polygon-matic-logo-1DFDA3A3A8-seeklogo.com.png" height={20} width={20} style={{ marginTop: 'auto', marginBottom: 'auto', marginRight: '5px' }}></img> MATIC ${maticPrice}</span>
+          <span className="days__compound" style={{ color: 'lightblue', width: 250 }}><img src="https://seeklogo.com/images/B/binance-coin-bnb-logo-CD94CC6D31-seeklogo.com.png" height={20} width={20} style={{ marginTop: 'auto', marginBottom: 'auto', marginRight: '5px' }}></img> BUSD ${busdPrice}</span>
+          <span className="days__compound" style={{ color: 'lightblue', width: 250 }}><img src="https://srockettoken.io/wp-content/uploads/2022/02/Group-97-150x150.png" height={20} width={20} style={{ marginTop: 'auto', marginBottom: 'auto', marginRight: '5px' }}></img> SROCKET ${stableOne}</span>
         </div>
       </Marquee>
 
